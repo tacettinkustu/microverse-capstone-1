fcb957680a65c59e756a676404d9fa0f9838d6b4
tacettinkustu <tacettinkustu>
2021-06-28 15:03:58 +0300

    Create featured speakers section

--- app.js
+++ app.js
@@ -1,3 +1,64 @@
+const speakers = [
+  {
+    id: 1,
+    image: 'https://avatars.githubusercontent.com/u/51737508?v=4',
+    name: 'Tacettin Kustu',
+    organization: 'Full Stack Developer at Microverse',
+    background:
+      'Lorem ipsum, dolor sit amet adipisicing elit. Deleniti, molestias.',
+  },
+  {
+    id: 2,
+    image:
+      'https://static.dezeen.com/uploads/2021/06/elon-musk-architect_dezeen_1704_col_0.jpg',
+    name: 'Elon Musk',
+    organization: 'CEO of Tesla',
+    background:
+      'Lorem ipsum, dolor sit amet adipisicing elit. Deleniti, molestias.',
+  },
+];
+
+const featuredSpeakers = document.querySelector('.speakers-list');
+
+eventListener();
+
+function eventListener() {
+  featuredSpeakers.addEventListener('DOMContentLoaded', loadSpeakers());
+}
+
+function loadSpeakers(e) {
+  speakers.forEach((speaker) => {
+    const speakerContainer = document.createElement('article');
+    speakerContainer.className = 'flex-row-center';
+    featuredSpeakers.appendChild(speakerContainer);
+
+    const speakerImage = document.createElement('img');
+    speakerImage.src = `${speaker.image}`;
+    speakerImage.alt = `Image of ${speaker.name}`;
+    speakerImage.className = 'speaker-image';
+    speakerContainer.appendChild(speakerImage);
+
+    const speakerInfos = document.createElement('div');
+    speakerInfos.className = 'speaker-infos';
+    speakerContainer.appendChild(speakerInfos);
+
+    const speakerName = document.createElement('h3');
+    speakerName.className = 'light-black heading-small';
+    speakerName.textContent = `${speaker.name}`;
+    speakerInfos.appendChild(speakerName);
+
+    const speakerOrganization = document.createElement('h4');
+    speakerOrganization.className = 'dark-orange';
+    speakerOrganization.textContent = `${speaker.organization}`;
+    speakerInfos.appendChild(speakerOrganization);
+
+    const speakerBackground = document.createElement('h4');
+    speakerBackground.className = 'light-black';
+    speakerBackground.textContent = `${speaker.background}`;
+    speakerInfos.appendChild(speakerBackground);
+  });
+}
+
 function openNav() {
   document.getElementById('myNav').style.width = '100%';
 }
